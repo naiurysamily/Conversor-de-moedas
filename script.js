443e242460ce3cf5ee1e713025a23addb81f2fd2
Naiury Samily <naiurysamily12@gmail.com>
2026-03-27 21:32:41 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const prevButton = document.getElementById('prev')
+const nextButton = document.getElementById('next')
+const items = document.querySelectorAll('.item')
+const dots = document.querySelectorAll('.dot')
+const numberIndicator = document.querySelector('.numbers')
+const list = document.querySelector('.list')
+
+let active = 0;
+const total = items.length;
+let timer;
+
+function update(direction) {
+
+document.querySelector('.item.active').classList.remove('active')
+document.querySelector('.dot.active').classList.remove('active')
+
+if(direction > 0){
+ active = active + 1
+
+ if(active === total){
+    active = 0
+ }
+} 
+
+else if(direction < 0 ) {
+
+
+}
+
+items[active].classList.add('active')
+dots[active].classList.add('active')
+
+}
+
+
+prevButton.addEventListener('click',() => {
+   update(-1)
+})
+
+nextButton.addEventListener('click',() => {
+    update(+1)
+})
